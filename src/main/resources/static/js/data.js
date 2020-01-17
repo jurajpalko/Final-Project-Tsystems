3047cd5191a6399a0b32dc41f3a3a7a07e456af3
student <student@DESKTOP-EFF88Q6>
2020-01-17 13:50:33 +0100

    added removing li elements in html

---
+++ src/main/resources/static/js/data.js
@@ -0,0 +1,4 @@
+var requirements = $('.select-requirements >p');
+var requirementsUL = $('.select-requirements >ul');
+$('span:empty:only-child').parent('p').remove();
+$("li:nth-child(n+3)").hide();
