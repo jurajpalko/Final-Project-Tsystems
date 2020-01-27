c573a88778c792b7d2d40d71668264a8b97126c4
Student <Student@DESKTOP-CFO3QQC>
2020-01-27 15:47:44 +0100

    fix to no date specified

--- src/main/resources/static/js/date.js
+++ src/main/resources/static/js/date.js
@@ -1,19 +1,22 @@
 var x = document.getElementsByClassName("format-date");
 var temp = Array();
 for (var i = 0; i < x.length; i++) {
-	  temp[i] = x[i].innerText;
-	  x[i].innerHTML = formatDate (new Date(Date.parse(temp[i])));
+	temp[i] = x[i].innerText;
+	if (temp[i] != (null || '')) {
+		x[i].innerHTML = formatDate(new Date(Date.parse(temp[i])));
+	}else{x[i].innerHTML = "open"}
 }
 function formatDate(date) {
-	
 
-	  var monthNames = [ "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" ];
+	var monthNames = [ "01", "02", "03", "04", "05", "06", "07", "08", "09",
+			"10", "11", "12" ];
 
-	  var day = date.getDate();
-	  
-	  if(day<10) day = "0"+day;
-	  var monthIndex = date.getMonth();
-	  var year = date.getFullYear();
+	var day = date.getDate();
 
-	  return day + '. ' + monthNames[monthIndex] + '. ' + year;
-	}
+	if (day < 10)
+		day = "0" + day;
+	var monthIndex = date.getMonth();
+	var year = date.getFullYear();
+
+	return day + '. ' + monthNames[monthIndex] + '. ' + year;
+}
