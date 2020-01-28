6a208270937284687d3aae44b4672d6d04c646b5
Juraj <palkojuraj5@gmail.com>
2020-01-28 10:00:11 +0100

    slider fixed

--- src/main/resources/static/js/slider.js
+++ src/main/resources/static/js/slider.js
@@ -33,297 +33,97 @@ showSlides();
  * */
 function showSlides() {
 
-
 	slideIndex++;
 
-	if (slideIndex > slides.length) {
-
-		slideIndex = 1;
-	}
-
-	slides[slideIndex - 1].style.display = "table-row";
-
-	if (slideIndex < 11) {
-		slides[slides.length - (11 - slideIndex)].style.display = "none";
-
+	if (slides.length < 11) {
+		for (var i = 1; i < slides.length; i++) {
+			slides[i - 1].style.display = "table-row";
+		}
 	} else {
-		slides[slideIndex - 11].style.display = "none";
 
-	}
-	
-	/* ON THE SCREEN 3 AND 4
-	 * ITERATES OVER A LIST OF POSTIONS DISPLAYING 2 POSITIONS AT A TIME WITH MORE INFO, SWITCHING SCREEN 3 AND SCREEN 4 FOR NEXT POSITION
-	 * SETTING DISPLAY-BLOCK FOR 1 POSITON AND DISPALY-NONE FOR PREVIOUS ONE BY ONE
-	 * */
-	
-	if (e==0) {
-		screens[e].style.display = "block";
-		screens[screens.length-2].style.display = "none";
-		if (side==true) {
-			screens[screens.length-2].className = "screens"
-			screens[e].className = "col-6 screen-three screens";
-			side=false;
-		}else{
-			screens[screens.length-2].className = "screens"
-			screens[e].className = "col-6 screen-four screens";
-			side=true;
+		if (slideIndex > slides.length) {
+
+			slideIndex = 1;
 		}
-		e++;
-	}
-	else if (e==1) {
-		screens[e].style.display = "block";
-		screens[screens.length-1].style.display = "none";
-		if (side==true) {
-			screens[screens.length-1].className = "screens"
-			screens[e].className = "col-6 screen-three screens";
-			side=false;
-		}else{
-			screens[screens.length-1].className = "screens"
-			screens[e].className = "col-6 screen-four screens";
-			side=true;
+		slides[slideIndex - 1].style.display = "table-row";
+
+		if (slideIndex < 11) {
+			slides[slides.length - (11 - slideIndex)].style.display = "none";
+
+		} else {
+			slides[slideIndex - 11].style.display = "none";
+
 		}
-		e++;
 	}
-	else{
-		screens[e].style.display = "block";
-		screens[e-2].style.display = "none";
-		if (side==true) {
-			screens[e-2].className = "screens"
-			screens[e].className = "col-6 screen-three screens";
-			side=false;
-		}else{
-			screens[e-2].className = "screens"
-			screens[e].className = "col-6 screen-four screens";
-			side=true;
-		}
-		e++;
-	}
-	if (e==screens.length) {
-		e=0;
-	}
+
+	positionSlider();
 
 	// TIME FOR ONE STEP AND SELF CALL
-	setTimeout(showSlides, 1000);
+	setTimeout(showSlides, 500);
 }
 
+/*
+ * ON THE SCREEN 3 AND 4 ITERATES OVER A LIST OF POSTIONS DISPLAYING 2
+ * POSITIONS AT A TIME WITH MORE INFO, SWITCHING SCREEN 3 AND SCREEN 4 FOR
+ * NEXT POSITION SETTING DISPLAY-BLOCK FOR 1 POSITON AND DISPALY-NONE FOR
+ * PREVIOUS ONE BY ONE
+ */
 
-
-
-
-
-
-
-
-//function showSlides() {
-//
-//	slideIndex++;
-//
-//	if (slideIndex > slides.length) {
-//
-//		slideIndex = 1;
-//	}
-//
-//	slides[slideIndex - 1].style.display = "table-row";
-//
-//	if (slideIndex < 9) {
-//		slides[slides.length - (9 - slideIndex)].style.display = "none";
-//
-//	} else {
-//		slides[slideIndex - 9].style.display = "none";
-//
-//	}
+function positionSlider() {
 
 	
-	
-	
-//	if (true) {
-//		if (count4==0) {
-//			screen3[count3.length].style.display = "none";;
-//		}
-//		screen3[count3].style.display = "block";;
-//		count3++;
-//		side!=side;
-//	}else{
-//		screen4[count4].style.display = "block";;
-//		count4++;
-//		side!=side;
-//	}
-//	
-//	
-//	if(count3=screen3.length-1){
-//		count3=0;
-//	}
-//	if(count4=screen4.length-1){
-//		count4=0;
-//	}
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	if (e==0) {
-//		
-//		if ((screen3.length+screen4.length)%2==0) {
-//			screen3[e].style.display = "block";
-//			e++;
-//		}
-//	}
-//	
-//	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	if (count3 == 0) {
-//		screen3[screen3.length-1].style.display = "none";
-//		screen3[count3].style.display = "block";
-//		console.log("e= "+e);
-//		console.log("screen3 count= "+count3);		
-//		e++;
-//		
-//	}
-//	if(count4==0){
-//		screen4[screen4.length-1].style.display = "none";
-//		screen4[count4].style.display = "block";
-//		console.log("e= "+e);
-//		console.log("screen4 count= "+count4);		
-//		e++;
-//	}
-//	if(count3!=0){
-//		screen3[count3-1].style.display = "none";
-//		screen3[count3].style.display = "block";
-//		console.log("e= "+e);
-//		console.log("screen3 count= "+count3);		
-//		e++;
-//
-//		
-//	}
-//	if(count4!=0){
-//		screen4[count4-1].style.display = "none";
-//		screen4[count4].style.display = "block";
-//		console.log("e= "+e);
-//		console.log("screen4 count= "+count4);		
-//		e++;
-//
-//	}
-//	count3++;
-//	count4++;
-//	
-//
-//
-//	if (count3 == screen3.length-1) {
-//		count3 =0;
-//		if ((screen3.length+screen4.length)%2>0) {
-//			e=0;
-//		}
-//
-//		
-//	}
-//	if (count4 == screen4.length-1) {
-//	screen3[count3].style.display = "block";
-//		count4 =0;
-//		if ((screen3.length+screen4.length)%2==0) {
-//			e=0;
-//		}
-//	}
-	
-	
+	if (slides.length < 3) {
+		screens[0].style.display = "block";
+		screens[0].className = "col-6 screen-three screens";
 
-//	setTimeout(showSlides, 1000);
-//}
+		screens[1].style.display = "block";
+		screens[1].className = "col-6 screen-four screens";
 
+	} else {
 
-	
-	
-	
-	
-	
-	
-	
+		if (e == 0) {
+			screens[e].style.display = "block";
+			screens[screens.length - 2].style.display = "none";
+			if (side == true) {
+				screens[screens.length - 2].className = "screens"
+				screens[e].className = "col-6 screen-three screens";
+				side = false;
+			} else {
+				screens[screens.length - 2].className = "screens"
+				screens[e].className = "col-6 screen-four screens";
+				side = true;
+			}
+			e++;
+		} else if (e == 1) {
+			screens[e].style.display = "block";
+			screens[screens.length - 1].style.display = "none";
+			if (side == true) {
+				screens[screens.length - 1].className = "screens"
+				screens[e].className = "col-6 screen-three screens";
+				side = false;
+			} else {
+				screens[screens.length - 1].className = "screens"
+				screens[e].className = "col-6 screen-four screens";
+				side = true;
+			}
+			e++;
+		} else {
+			screens[e].style.display = "block";
+			screens[e - 2].style.display = "none";
+			if (side == true) {
+				screens[e - 2].className = "screens"
+				screens[e].className = "col-6 screen-three screens";
+				side = false;
+			} else {
+				screens[e - 2].className = "screens"
+				screens[e].className = "col-6 screen-four screens";
+				side = true;
+			}
+			e++;
+		}
+		if (e == screens.length) {
+			e = 0;
+		}
 
-//function showSlides() {
-//
-////	console.log("0i= "+e);
-//	slideIndex++;
-//
-//	if (slideIndex > slides.length) {
-//
-//		slideIndex = 1;
-//	}
-//
-//	slides[slideIndex - 1].style.display = "table-row";
-//
-//	if (slideIndex < 9) {
-//		slides[slides.length - (9 - slideIndex)].style.display = "none";
-//
-//	} else {
-//		slides[slideIndex - 9].style.display = "none";
-//
-//	}
-//	
-//	
-//	
-////console.log("1i= "+e);
-////console.log("1count= "+count);
-////								if (e == 0) {
-////									screen3[e].style.display = "block";
-////									count3++;
-////									console.log("screen3 count= "+count3);
-////									screen4[e].style.display = "block";
-////									count4++;
-////									console.log("screen4 count= "+count4);
-////								}
-////								else{
-////									console.log(e - 1);
-////									screen3[e - 1].style.display = "none";
-////									screen3[e].style.display = "block";
-////									count3++;
-////									console.log("screen3 count= "+count3);
-////							//		console.log("3count= "+count);
-////									screen4[e - 1].style.display = "none";
-////									screen4[e].style.display = "block";
-////									count4++;	
-////									console.log("screen4 count= "+count4);
-////							//		console.log("4count= "+count);
-////								}
-////								
-////							
-////							e++;
-////						console.log("e= "+e);
-////						
-////							if (count3 == screen3.length-1) {
-////								count3 =0;
-////								
-////							}
-////							if (count4 == screen4.length-1) {
-////								count4 =0;
-////								
-////							}
-////							if (e == count3+count4) {
-////								e=0;
-////							}
-////	console.log("3i= "+e);
-////	console.log("5count= "+count);
-//	setTimeout(showSlides, 1000);
-//}
+	}
+}
